21d1e530f9e1447f10d81603e604eae18f0925f7
LoicProust <loic.proust.hoppeler@gmail.com>
2017-02-16 17:07:36 +0100

    Implement very first playable version of the game

--- MazeSolver/MazeSolver/GameHelper.cs
+++ MazeSolver/MazeSolver/GameHelper.cs
@@ -3,53 +3,158 @@ using MazeSolver.ServiceReference1;
 
 namespace MazeSolver
 {
-	public class GameHelper
-	{
+    public class GameHelper
+    {
 
-		private GameClient gameClient { get; set; }
-		private PlayerGame playerGame { get; set; }
-		private Game game { get; set; }
+        private GameClient gameClient { get; set; }
+        private PlayerGame playerGame { get; set; }
+        private Game game { get; set; }
+        private string[][] map { get; set; }
+        private bool isManualMode { get; set; }
 
-		public GameHelper()
-		{
-		}
+        private Position lastPosition { get; set; }
+        private Position[] walkableCells { get; set; }
 
-		public void initNewGame(Difficulty difficulty, string playerName)
-		{
-			gameClient = new GameClient();
-			playerGame = gameClient.CreateGame(difficulty, playerName);
-			game = gameClient.LoadGame(playerGame.Key);
+        public GameHelper()
+        {
+        }
 
-		}
+        public void initNewGame(Difficulty difficulty, string playerName)
+        {
+            gameClient = new GameClient();
+            playerGame = gameClient.CreateGame(difficulty, playerName);
+            game = gameClient.LoadGame(playerGame.Key);
+            //mode // NOt use at the moment
+            initMap();
+        }
 
 
-		public int[] getDimensions()
-		{
-			int[] dimensions = new int[2];
-			dimensions[0] = game.Maze.Width;
-			dimensions[1] = game.Maze.Height;
-			return dimensions;
-		}
+        public int[] getDimensions()
+        {
+            int[] dimensions = new int[2];
+            dimensions[0] = game.Maze.Width;
+            dimensions[1] = game.Maze.Height;
+            return dimensions;
+        }
 
-		public string[][] getVisiblesCells()
-		{
-			//List<List<string>> map = new List<List<string>>();
-			//string[,] chess2 = new string[20,20];
-			string[][] map = new string[game.Maze.Height][];
-			for (int i = 0; i < game.Maze.Height; i++)
-			{
-				map[i] = new string[game.Maze.Width];
-			}
-			foreach (Cell cell in playerGame.Player.VisibleCells)
-			{
-				map[cell.Position.X][cell.Position.Y] = cell.CellType.ToString().Substring(0, 1);
-			}
-			return map;
-		}
+        public string[][] getVisiblesCells() //zsqd
+        {
+            foreach (Cell cell in playerGame.Player.VisibleCells)
+            {
+                map[cell.Position.Y][cell.Position.X] = cell.CellType.ToString().Substring(0, 1);
+                Console.WriteLine(cell.Position.Y + " " + cell.Position.X);
+                if (cell.CellType == CellType.Empty)
+                {
+                    map[cell.Position.Y][cell.Position.X] = " ";
+                }
+                if (playerGame.Player.CurrentPosition.X == cell.Position.X && playerGame.Player.CurrentPosition.Y == cell.Position.Y)
+                {
+                    map[cell.Position.Y][cell.Position.X] = "¤";
+                    if (cell.CellType == CellType.End)
+                    {
+                        map[cell.Position.Y][cell.Position.X] = "?";
+                    }
+                }
+            }
+            return map;
+        }
 
-		public void closeGame()
-		{
-			gameClient.Close();
-		}
-	}
+        public Position[] getWalkableCell()
+        {
+
+            foreach (Cell cell in playerGame.Player.VisibleCells)
+            {
+                if (playerGame.Player.CurrentPosition.X == cell.Position.X && playerGame.Player.CurrentPosition.Y == cell.Position.Y - 1 ||
+                    playerGame.Player.CurrentPosition.X == cell.Position.X && playerGame.Player.CurrentPosition.Y == cell.Position.Y + 1 ||
+                    playerGame.Player.CurrentPosition.X - 1 == cell.Position.X && playerGame.Player.CurrentPosition.Y == cell.Position.Y ||
+                    playerGame.Player.CurrentPosition.X + 1 == cell.Position.X && playerGame.Player.CurrentPosition.Y == cell.Position.Y)
+                {
+
+                }
+            }
+            return null;
+        }
+
+        public bool move(Direction direction)
+        {
+            int tmpX = playerGame.Player.CurrentPosition.X;
+            int tmpY = playerGame.Player.CurrentPosition.Y;
+
+            if (verifiyDestination(direction))
+            {
+                playerGame.Player = gameClient.MovePlayer(game.Key, playerGame.Player.Key, direction);
+
+                if (playerGame.Player.CurrentPosition.X == tmpX && playerGame.Player.CurrentPosition.Y == tmpY)
+                {
+                    return false;
+                }
+                return true;
+
+            }
+            else //verifiyDestination == false
+            {
+                Console.WriteLine("Impossible de se déplacer dans cette direction");
+                return false;
+            }
+        }
+
+        public void autoStraightMove(Direction direction)
+        {
+            bool isWallInFront = move(direction);
+
+        }
+
+
+        public bool verifiyDestination(Direction direction)
+        {
+            if (direction == Direction.Right && "W" !=
+                getVisiblesCells()[playerGame.Player.CurrentPosition.Y][playerGame.Player.CurrentPosition.X + 1])
+            {
+                return true;
+            }
+            else if (direction == Direction.Left && "W" !=
+               getVisiblesCells()[playerGame.Player.CurrentPosition.Y][playerGame.Player.CurrentPosition.X - 1])
+            {
+                return true;
+            }
+            else if (direction == Direction.Up && "W" !=
+              getVisiblesCells()[playerGame.Player.CurrentPosition.Y - 1][playerGame.Player.CurrentPosition.X])
+            {
+                return true;
+            }
+            else if (direction == Direction.Down && "W" !=
+              getVisiblesCells()[playerGame.Player.CurrentPosition.Y + 1][playerGame.Player.CurrentPosition.X])
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string getEndTag()
+        {
+            return playerGame.Player.SecretMessage;
+        }
+
+        private void initMap()
+        {
+            map = new string[playerGame.Maze.Height][];
+            for (int i = 0; i < playerGame.Maze.Height; i++)
+            {
+                map[i] = new string[playerGame.Maze.Width];
+            }
+            for (int i = 0; i < playerGame.Maze.Height; i++)
+            {
+                for (int j = 0; j < playerGame.Maze.Width; j++)
+                {
+                    map[i][j] = " ";
+                }
+            }
+        }
+
+        public void closeGame()
+        {
+            gameClient.Close();
+        }
+
+    }
 }
--- MazeSolver/MazeSolver/Program.cs
+++ MazeSolver/MazeSolver/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MazeSolver.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,144 @@ namespace MazeSolver
 {
     class Program
     {
+        private static GameHelper gameHelper { get; set; }
+
         static void Main(string[] args)
         {
+
+            gameHelper = new GameHelper();
+
+            Difficulty difficulty = chooseDifficulty();
+            //bool mode = isManualMode();
+            gameHelper.initNewGame(difficulty, "Player1");
+            Console.WriteLine("\n/* Partie " + difficulty + " commencée *\\\n");
+
+            displayMap(gameHelper.getVisiblesCells());
+            System.Threading.Thread.Sleep(500);
+
+            /*if (mode)
+            {*/
+                runManualMode();
+            /*}
+            else
+            {
+                runAutoMode();
+            }*/
+
+            gameHelper.closeGame();
+        }
+
+        private static bool displayMap(string[][] map)
+        {
+            bool isFinished = false;
+            foreach (string[] x in map)
+            {
+                string tmpString = "";
+                foreach (string xy in x)
+                {
+                    if (xy == "?")
+                    {
+                        isFinished = true;
+                    }
+                    tmpString += " " + xy;
+                }
+                Console.WriteLine(tmpString);
+            }
+            return isFinished;
+        }
+
+        private static Difficulty chooseDifficulty()
+        {
+            Console.WriteLine("Difficulté ?");
+            Console.WriteLine(" 1 : Easy");
+            Console.WriteLine(" 2 : Medium");
+            Console.WriteLine(" 3 : Hard");
+            Console.WriteLine(" 4 : Extreme");
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        return Difficulty.Easy;
+                    case "2":
+                        return Difficulty.Medium;
+                    case "3":
+                        return Difficulty.Hard;
+                    case "4":
+                        return Difficulty.Extreme;
+                }
+            } while (input != "1" &&
+                input != "2" &&
+                input != "3" &&
+                input != "4");
+            return Difficulty.Extreme;
+        }
+
+        private static bool isManualMode()
+        {
+            Console.WriteLine("Mode manuel ?");
+            Console.WriteLine(" 1 : Manuel");
+            Console.WriteLine(" 2 : Auto");
+            string input;
+            do
+            {
+                input = Console.ReadLine();
+                switch (input)
+                {
+                    case "1":
+                        return true;
+                    case "2":
+                        return false;
+                }
+            } while (input != "1" && input != "2");
+            return true;
+        }
+
+
+        private static void runManualMode()
+        {
+            string input = Console.ReadLine();
+            while (input != "quit" && input != "exit")
+            {
+                switch (input)
+                {
+                    case "d":
+                        gameHelper.move(Direction.Right);
+                        break;
+                    case "q":
+                        gameHelper.move(Direction.Left);
+                        break;
+                    case "z":
+                        gameHelper.move(Direction.Up);
+                        break;
+                    case "s":
+                        gameHelper.move(Direction.Down);
+                        break;
+                    default:
+                        Console.WriteLine("\n\nVeuillez entrer un input valide\n");
+                        break;
+                }
+                if (displayMap(gameHelper.getVisiblesCells()))
+                {
+                    Console.WriteLine("\nFin du labyrinthe trouvée : ");
+                    Console.WriteLine(gameHelper.getEndTag());
+
+                }
+                System.Threading.Thread.Sleep(500);
+                input = Console.ReadLine();
+            }
+
+        }
+
+        private static void runAutoMode()
+        {
+            bool endIsReached = false;
+            do
+            {
+
+            } while (!endIsReached);
         }
     }
 }
